131646cb4b5c8f8a434973c030d5422fa25bf1be
VeremyevVadim <qwertyvadimv@mail.ru>
2020-10-28 16:07:26 +0300

    Task 4
    Run animation. After 1 sec run slower

---
+++ AnimatorTask/Assets/LayersTask/Scripts/RunBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class RunBehavior : StateMachineBehaviour
+{
+    private float _baseAnimSpeed = 1;
+    private float _deltaTime = 0f;
+    
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _baseAnimSpeed = animator.speed;
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _deltaTime += Time.deltaTime;
+        if (_deltaTime > 1f)
+        {
+            animator.speed = _baseAnimSpeed / 2f;
+        }
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.speed = _baseAnimSpeed;
+        _deltaTime = 0f;
+    }
+}
